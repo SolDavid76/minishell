3de9e3bd66ec71792eff152319322a8d54207a0d
boby-booba <enzo.nollet@gmail.com>
2023-06-19 12:15:26 +0200

    try debug

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,11 +6,13 @@
 /*   By: ennollet <ennollet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 10:57:05 by ennollet          #+#    #+#             */
-/*   Updated: 2023/06/19 11:28:31 by ennollet         ###   ########.fr       */
+/*   Updated: 2023/06/19 12:14:47 by ennollet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+extern t_shell	*g_shell;
 
 void env(char **envp)
 {
@@ -19,7 +21,8 @@ void env(char **envp)
 	i = 0;
 	while (envp[i])
 	{
-		printf("%s\n", envp[i]);
+		// printf("%s\n", envp[i]);
+		write(1, envp[i], ft_strlen(envp[i]));
 		i++;
 	}
 }
