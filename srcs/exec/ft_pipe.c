1942dfc373fc507ec28e0273937fd29b2eedb096
SolDavid_76 <david.janusz75@gmail.com>
2023-06-21 12:39:50 +0200

    exec v1.0

--- srcs/exec/ft_pipe.c
+++ srcs/exec/ft_pipe.c
@@ -6,7 +6,7 @@
 /*   By: djanusz <djanusz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 10:23:37 by djanusz           #+#    #+#             */
-/*   Updated: 2023/06/20 18:01:08 by djanusz          ###   ########.fr       */
+/*   Updated: 2023/06/21 12:38:54 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ void	ft_pipe_child_redirection(t_pipe *pipeline, char **cmd)
 	}
 	if (pipeline->i != pipeline->nbcmd - 1)
 		dup2(pipeline->fd[1], 1);
-	g_shell->exit_value = 0;
 	ft_redirection(cmd);
 	close(pipeline->fd[0]);
 	close(pipeline->fd[1]);
@@ -45,23 +44,25 @@ void	ft_pipe_child(t_pipe *pipeline, char **cmd, char **envp)
 	signal(SIGQUIT, SIG_DFL);
 	free(pipeline->pid);
 	ft_pipe_child_redirection(pipeline, cmd);
-	if (g_shell->exit_value == 0 && is_buildin(cmd[0]))
+	if (g_shell->exit_value != 0)
+		ft_main_exit(g_shell->exit_value);
+	if (is_buildin(cmd[0]))
 		exec_buildin(cmd, 1);
-	if (g_shell->exit_value == 0 && access(cmd[0], X_OK) == 0)
+	if (access(cmd[0], X_OK) == 0)
 		execve(cmd[0], cmd, envp);
+	else
+	{
+		write(2, cmd[0], ft_strlen(cmd[0]));
+		write(2, ": command not found\n", 20);
+		ft_main_exit(127);
+	}
 	if (g_shell->exit_value)
 		ft_main_exit(g_shell->exit_value);
 	ft_main_exit(errno);
 }
 
-void	ft_pipe_parent(t_pipe *pipeline, char **cmd)
+void	ft_pipe_parent(t_pipe *pipeline)
 {
-	if (access(cmd[0], X_OK) == -1 && !is_buildin(cmd[0]))
-	{
-		g_shell->exit_value = 127;
-		write(2, cmd[0], ft_strlen(cmd[0]));
-		write(2, ": command not found\n", 20);
-	}
 	close(pipeline->fd[1]);
 	if (pipeline->prev_pipe != -1)
 		close(pipeline->prev_pipe);
@@ -86,7 +87,7 @@ void	ft_pipe(char ***cmds, char **envp)
 		pipeline.pid[pipeline.i] = fork();
 		if (pipeline.pid[pipeline.i] == 0)
 			ft_pipe_child(&pipeline, cmds[pipeline.i], envp);
-		ft_pipe_parent(&pipeline, cmds[pipeline.i]);
+		ft_pipe_parent(&pipeline);
 		pipeline.i++;
 	}
 	close(pipeline.prev_pipe);
